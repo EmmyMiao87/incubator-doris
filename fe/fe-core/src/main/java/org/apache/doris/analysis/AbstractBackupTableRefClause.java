211c87c218a0bf5eba4aadcc2e9fdede1b80ea9e
EmmyMiao87 <522274284@qq.com>
2021-01-29 10:36:32 +0800

    Fix bug

--- fe/fe-core/src/main/java/org/apache/doris/analysis/AbstractBackupTableRefClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/AbstractBackupTableRefClause.java
@@ -61,7 +61,7 @@ public class AbstractBackupTableRefClause implements ParseNode {
             tableRefList.add(tableRef);
         }
 
-        LOG.debug("table refs after normalization: \n{}", Joiner.on("\n").join(tableRefList));
+        LOG.debug("table refs after normalization: {}", Joiner.on(",").join(tableRefList));
     }
 
     public boolean isExclude() {
